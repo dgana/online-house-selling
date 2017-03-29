90e92e0b964c065323d4dfe2c3bbe6a70122d716
dgana <dhegana@gmail.com>
2017-03-29 15:31:54 +0700

    fix seeder

--- server/seeder/houseSeed.js
+++ server/seeder/houseSeed.js
@@ -6,7 +6,7 @@ module.exports = [
     image: 'https://s-media-cache-ak0.pinimg.com/736x/73/de/32/73de32f9e5a0db66ec7805bb7cb3f807.jpg',
     lat: '-6.23606632361087',
     lng: '106.782959997654',
-    price: 200000000000
+    price: '200000000000'
   },
   {
     name: 'Adhy Wiranata',
@@ -15,6 +15,6 @@ module.exports = [
     image: 'https://s-media-cache-ak0.pinimg.com/originals/34/3a/96/343a96a7cdd373f36b2ab0580f46af84.jpg',
     lat: '-8.23606632361087',
     lng: '102.782959997654',
-    price: 150000000000
+    price: '150000000000'
   }
 ]
